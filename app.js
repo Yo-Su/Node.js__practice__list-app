46fa0539398b5c74b7dc3d93a5cde4fc403cf81c
Yo-Su <57243991+Yo-Su@users.noreply.github.com>
2020-03-07 13:13:25 +0900

    add リストの表示、リンクの追加 ejsファイルでの表示方法、リンク方法

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ const app = express();
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
+app.get('/hello', (req, res) => {
   res.render('hello.ejs');
 });
 
-app.get('/top', (req, res) => {
+app.get('/', (req, res) => {
   res.render('top.ejs');
+});
+
+app.get('/index', (req, res) => {
+  res.render('index.ejs');
 });
 
 app.listen(3000);
